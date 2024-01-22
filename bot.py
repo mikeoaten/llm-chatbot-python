474fdb9a1772778ed6bb8f1dcc93c04aabc9f5db
mikeoaten123 <68006148+mikeoaten123@users.noreply.github.com>
2024-01-22 09:43:53 +0000

    tidy up

--- bot.py
+++ bot.py
@@ -19,6 +19,20 @@ st.set_page_config(page_title="RNS Buddy", page_icon=":newspaper:")
 st.header("RNS buddy")
 
 
+def write_message(role, content, save=True):
+    """
+    This is a helper function that saves a message to the
+     session state and then writes a message to the UI
+    """
+    # Append to session state
+    if save:
+        st.session_state.messages.append({"role": role, "content": content})
+
+    # Write to UI
+    with st.chat_message(role):
+        st.markdown(content)
+
+
 # Set up Session State
 if "messages" not in st.session_state:
     st.session_state.messages = [
